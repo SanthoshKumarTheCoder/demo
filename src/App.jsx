99cec9f4df66fd147479e6ba352c0bc4c4d97000
SANTHOSH KUMAR.G <sksanthoshkumarsk007@gmail.com>
2025-09-13 12:34:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@
 import './App.css'
 import AnalyticsDashboard from './component/AnalyticsDashboard/AnalyticsDashboard'
 import InvoiceTemplate from './component/InvoiceTemplate/InvoiceTemplate'
-import PaymentForm from './component/PaymentForm/PaymentForm'
 import PlanPayment from './component/PlanPayment/PlanPayment'
 import ReportGenerator from './component/ReportGenerator/ReportGenerator'
 
